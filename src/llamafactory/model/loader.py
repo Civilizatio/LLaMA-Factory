3d666f4ef5b478efbdae68bd0605aca4f1023042
Yaser Afshar <yaser.afshar@intel.com>
2024-10-25 10:15:42 -0700

    Add missing key to init_kwargs

--- src/llamafactory/model/loader.py
+++ src/llamafactory/model/loader.py
@@ -157,6 +157,7 @@ def load_model(
             if model_args.train_from_scratch:
                 model = load_class.from_config(config, trust_remote_code=model_args.trust_remote_code)
             else:
+                init_kwargs["trust_remote_code"] = model_args.trust_remote_code
                 model = load_class.from_pretrained(**init_kwargs)
 
         if model_args.mixture_of_depths == "convert":
